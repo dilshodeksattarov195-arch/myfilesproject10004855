c9dd544eda020c31e074dc4a263b4dac432c4ed8
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-29 18:11:21 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class orderVaveWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = orderVaveWorker()
+    print("Worker engine initialized.")
